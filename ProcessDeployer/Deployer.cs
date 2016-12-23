2a257d26e16320e9f6b90e7013e7c1debf37ef0d
Igor Polouektov <igorpol@gmail.com>
2016-12-23 12:42:36 -0500

    Added Deployer project

---
+++ ProcessDeployer/Deployer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KlaudWerk.ProcessEngine.Definition;
+using KlaudWerk.ProcessEngine.Persistence;
+
+namespace Klaudwerk.ProcessDeployer
+{
+    public class Deployer
+    {
+        private readonly List<IProcessDefinitionRegistry> _registries=new List<IProcessDefinitionRegistry>();
+        private readonly ProcessDefinitionPersisnenceService _persistence=new ProcessDefinitionPersisnenceService();
+
+        public IEnumerable<IProcessDefinitionRegistry> Registries
+        {
+            get { return _registries; }
+            set
+            {
+                _registries.Clear();
+                _registries.AddRange(value);
+                _registries.ForEach(r =>
+                {
+                    string[] errors;
+                    r.TryInitialize(out errors);
+                });
+            }
+        }
+        /// <summary>
+        /// List of assemblies
+        /// </summary>
+        public IEnumerable<ProcessDescriptor> Processes {
+            get
+            {
+                List<ProcessDescriptor> pd=new List<ProcessDescriptor>();
+                Registries?.ToList().ForEach(r=>pd.AddRange(r.Processes));
+                return pd;
+            }
+        }
+
+        public IEnumerable<ProcessDefinitionDigest> DeployedWorkflows => _persistence.LisAlltWorkflows();
+
+        public void DeployWorkflow(Guid guid)
+        {
+            List<ProcessDefinition> defs=new List<ProcessDefinition>();
+            _registries.ForEach(r =>
+            {
+                ProcessDefinition pd;
+                if (r.TryGet(guid, out pd))
+                    defs.Add(pd);
+            });
+            if (defs.Count == 0)
+                throw new ArgumentException($"No Process Definition with ID={guid} was found.");
+            if (defs.Count > 1)
+                throw new ArgumentException($"{defs.Count} Process Definitions with ID={guid} was found.");
+            Md5CalcVisitor visitor=new Md5CalcVisitor();
+            defs[0].Accept(visitor);
+            string md5 = visitor.CalculateMd5();
+            IReadOnlyList<ProcessDefinitionDigest> processes = _persistence.LisAlltWorkflows();
+            var processDefinitionDigests = processes.Where(p => p.Id == guid).ToArray();
+            int nextVersion = processDefinitionDigests.Length>0?processDefinitionDigests.Max(p => p.Version)+1:1;
+            foreach (ProcessDefinitionDigest p in processDefinitionDigests)
+            {
+                 if(p.Md5==md5)
+                     throw new ArgumentException($"Process ID={defs[0].Id} MD5={md5} already deployed. Cannot deploy the same process with the same MD5.");
+            }
+            foreach (ProcessDefinitionDigest p in processes)
+            {
+                _persistence.SetStatus(p.Id, p.Version, ProcessDefStatusEnum.NotActive);
+            }
+            _persistence.Create(defs[0],ProcessDefStatusEnum.Active,nextVersion);
+        }
+
+        public void SetWorkflowStatus(Guid guid, int version, ProcessDefStatusEnum status)
+        {
+            _persistence.SetStatus(guid, version, status);
+        }
+    }
+}
---
+++ ProcessDeployer/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Fclp;
+using KlaudWerk.ProcessEngine.Definition;
+using KlaudWerk.ProcessEngine.Persistence;
+
+namespace Klaudwerk.ProcessDeployer
+{
+    public class ApplicationArgumments
+    {
+        public string Command { get; set; }
+        public string FlowId { get; set; }
+        public int Version { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Command} : id={FlowId} version={Version}";
+        }
+    }
+    internal class Program
+    {
+
+        private static readonly Deployer WfDeployer=new Deployer();
+        private static readonly Dictionary<string,Action<ApplicationArgumments>>
+            Commands=new Dictionary<string, Action<ApplicationArgumments>>
+            {
+                {"help",PrintHelp},
+                {"h",PrintHelp},
+                {"?",PrintHelp},
+                {"la",ListWorkflowasInAssemblies},
+                {"listassemblies",ListWorkflowasInAssemblies},
+                {"ld",ListdeployedWorkflows},
+                {"listdeployed",ListdeployedWorkflows},
+                {"d",DeployWorkflow},
+                {"deploy",DeployWorkflow},
+                {"u",UndeployWorkflow},
+                {"undeploy",UndeployWorkflow},
+                {"a",ActivateWorkflow},
+                {"activate",ActivateWorkflow},
+                {"x",DeactivateWorkflow},
+                {"deactivate",DeactivateWorkflow},
+            };
+
+        public static void PrintHelp(ApplicationArgumments a)
+        {
+            Console.WriteLine("use -c <command> [-f <flowguid>] [-v <version>]");
+            string commands = @"
+commands:
+ h,? or help          - print help
+ la or listassemblies - list workflows in assemblies catalog
+ ld or listdeployed   - list deployed workflowa
+ d  or deploy         - deploy a workflow
+ u  or undeploy       - undeploy a workflow
+ a  or activate       - activate workflow
+ x  or deactivate     - deactivate workflow
+";
+          Console.WriteLine(commands);
+        }
+
+        public static void ListWorkflowasInAssemblies(ApplicationArgumments a)
+        {
+            Console.WriteLine("ID                                  |MD5                     |Name - Description");
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            WfDeployer.Processes.ToList().ForEach(Console.WriteLine);
+        }
+
+        public static void ListdeployedWorkflows(ApplicationArgumments a)
+        {
+            Console.WriteLine("ID                                  |MD5                     |Version|Status|Name - Description");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            WfDeployer.DeployedWorkflows.ToList().ForEach(w =>
+            {
+                string str = $"{w.Id}|{w.Md5}|{w.Version}|{w.Status}|{w.Name}-{w.Description} {w.LastUpdated}";
+                Console.WriteLine(str);
+            });
+        }
+
+        public static void DeployWorkflow(ApplicationArgumments a)
+        {
+            Guid guid = CheckId(a);
+            if (guid == Guid.Empty)
+            {
+                Console.WriteLine("Valid Process Definition GUID required.");
+                return;
+            }
+            try
+            {
+                WfDeployer.DeployWorkflow(guid);
+                Console.WriteLine($"Process ID={guid} has been deployed.");
+                ListdeployedWorkflows(a);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deploying process ID={guid}. Error={ex.Message}");
+            }
+
+
+        }
+
+        public static void UndeployWorkflow(ApplicationArgumments a)
+        {
+            DeactivateWorkflow(a);
+        }
+
+        public static void ActivateWorkflow(ApplicationArgumments a)
+        {
+            SetWorkflowStatius(a,ProcessDefStatusEnum.Active);
+            ListdeployedWorkflows(a);
+        }
+
+        public static void DeactivateWorkflow(ApplicationArgumments a)
+        {
+            SetWorkflowStatius(a, ProcessDefStatusEnum.NotActive);
+            ListdeployedWorkflows(a);
+        }
+
+        private static void SetWorkflowStatius(ApplicationArgumments a, ProcessDefStatusEnum status)
+        {
+            Guid guid = CheckId(a);
+            if (guid == Guid.Empty)
+            {
+                Console.WriteLine("Valid Process Definition GUID required.");
+                return;
+            }
+            if (a.Version == -1)
+            {
+                Console.WriteLine("Version number is required.");
+                return;
+            }
+            WfDeployer.SetWorkflowStatus(guid, a.Version, status);
+        }
+
+        private static Guid CheckId(ApplicationArgumments a)
+        {
+            if (string.IsNullOrEmpty(a.FlowId))
+            {
+                return Guid.Empty;
+            }
+            Guid guid;
+            return Guid.TryParse(a.FlowId, out guid) ? guid : Guid.Empty;
+        }
+
+        public static void Main(string[] args)
+        {
+            // Initialize the composition catalog
+            var aggregationPath = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "Workflows");
+            if (Directory.Exists(aggregationPath))
+            {
+                var directoryCatalog = new DirectoryCatalog(aggregationPath);
+                using (CompositionContainer container = new CompositionContainer(directoryCatalog))
+                {
+                    IEnumerable<IProcessDefinitionRegistry> registries =
+                        container.GetExportedValues<IProcessDefinitionRegistry>();
+                    WfDeployer.Registries = registries;
+                }
+            }
+
+            var parser = new FluentCommandLineParser<ApplicationArgumments>();
+            parser.Setup(a => a.Command).As('c', "command").Required().SetDefault("help");
+            parser.Setup(a => a.FlowId).As('f', "flowid").SetDefault("");
+            parser.Setup(a => a.Version).As('v', "version").SetDefault(-1);
+            var result = parser.Parse(args);
+            if (!result.HasErrors)
+            {
+                Action<ApplicationArgumments> action;
+                if (!Commands.TryGetValue(parser.Object.Command, out action))
+                {
+                    PrintHelp(parser.Object);
+                    return;
+                }
+                action(parser.Object);
+            }
+            else
+            {
+                Console.WriteLine(result.ErrorText);
+                Console.WriteLine("Use -c help for help");
+            }
+        }
+
+    }
+}
