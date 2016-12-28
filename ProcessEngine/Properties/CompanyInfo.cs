4a66ecd217eafd2a879aa1acde84dfc58777f483
ashalabad <igorpol@gmail.com>
2016-12-28 11:35:51 -0500

    Added MIT license headers; Company Info Assembly information has been moved to separatw file

---
+++ ProcessEngine/Properties/CompanyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("KlaudWerk")]
+[assembly: AssemblyProduct("ProcessEngine")]
+[assembly: AssemblyCopyright("Copyright © Igor Polouektov, 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
