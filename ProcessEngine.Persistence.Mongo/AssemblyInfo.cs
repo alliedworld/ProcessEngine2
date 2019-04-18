9deb1d081a81e1f946db48972619989bdcf59ca9
Igor Polouektov <igor.polouektov@awacservices.com>
2019-04-18 11:26:19 -0400

    version incremented

--- ProcessEngine.Persistence.Mongo/AssemblyInfo.cs
+++ ProcessEngine.Persistence.Mongo/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProcessDeployer")]
+[assembly: AssemblyTitle("Klaudwerk.ProcessEngine.Persistence.Mongo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("88370fd1-3cec-430e-985c-47edc1c91e97")]
+[assembly: Guid("816769c1-b99e-45ab-9ddc-f63e29688cf6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -24,8 +24,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
